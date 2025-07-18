5911276bfd651915550541694e2bff1d887666fe
Pradika <138282647+Pradikaa13@users.noreply.github.com>
2025-07-18 20:23:36 +0700

    Delete RegisterService.java
    Commit changes

--- RegisterService.java
+++
@@ -1,7 +0,0 @@
-package com.javaboy.javaboy_rest_api.core.service;
-
-import com.javaboy.javaboy_rest_api.dto.validasi.RegisterRequest;
-
-public interface RegisterService {
-    void registerUser(RegisterRequest registerRequest);
-}
