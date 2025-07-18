10c8683783563f46db27d1d8c38fe93415faed75
Pradika <138282647+Pradikaa13@users.noreply.github.com>
2025-07-18 20:23:02 +0700

    Delete LoginService.java
    Commit changes

--- LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.javaboy.javaboy_rest_api.core.service;
-
-import com.javaboy.javaboy_rest_api.dto.validasi.LoginRequest;
-
-public interface LoginService {
-    void login(LoginRequest loginRequest);
-    void forgotPassword(String email);
-    void rememberMe(String email, boolean remember);
-}
