b6399347adffe8f9f5c89eee6dae639339ca04a6
Pradika <138282647+Pradikaa13@users.noreply.github.com>
2025-07-18 20:23:51 +0700

    Delete ResetPasswordService.java
    Commit changes

--- ResetPasswordService.java
+++
@@ -1,7 +0,0 @@
-package com.javaboy.javaboy_rest_api.core.service;
-
-import com.javaboy.javaboy_rest_api.dto.validasi.ForgotPasswordRequest;
-
-public interface ResetPasswordService {
-    void sendResetLink(ForgotPasswordRequest request);
-}
